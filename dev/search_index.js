867c75616b81add0f267ddeb1b21ef3ff3df494e
Documenter.jl <documenter@juliadocs.github.io>
2022-08-09 22:45:30 +0000

    build based on 66c14d9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PlotIter","category":"page"},{"location":"#PlotIter","page":"Home","title":"PlotIter","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PlotIter.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PlotIter]","category":"page"}]
+}
